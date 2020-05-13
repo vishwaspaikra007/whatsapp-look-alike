4380f4efff3f4cb5e01302611245bf9b0b1bdb2c
Vishwas Paikra <vishwaspaikra007@gmail.com>
2020-05-14 04:02:01 +0530

    Updates

---
+++ precache-manifest.25bfd0abd0e12f477569c24fa096884c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1f8bc3c4a601fa3c7e7aec5112b031",
+    "url": "/whatsapp-look-alike/index.html"
+  },
+  {
+    "revision": "e2bbb414b8ccded4cd90",
+    "url": "/whatsapp-look-alike/static/css/main.5848aec7.chunk.css"
+  },
+  {
+    "revision": "13affbe4f754a845a6f5",
+    "url": "/whatsapp-look-alike/static/js/2.25a87ea5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whatsapp-look-alike/static/js/2.25a87ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2bbb414b8ccded4cd90",
+    "url": "/whatsapp-look-alike/static/js/main.f8334108.chunk.js"
+  },
+  {
+    "revision": "de73dd5e81ddfc0b99b0",
+    "url": "/whatsapp-look-alike/static/js/runtime-main.bfc79f2c.js"
+  },
+  {
+    "revision": "ef73c5cb19918fed9a4614f57ea5b6f0",
+    "url": "/whatsapp-look-alike/static/media/icons8-search.ef73c5cb.svg"
+  },
+  {
+    "revision": "db60cc1c6c53d33c46299cf632fce48f",
+    "url": "/whatsapp-look-alike/static/media/icons8-unsplash.db60cc1c.svg"
+  },
+  {
+    "revision": "f6557f75e77e0eeb2a190e42093fdbf9",
+    "url": "/whatsapp-look-alike/static/media/vertical-menu.f6557f75.svg"
+  }
+]);
