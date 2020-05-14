afaccf539643dc9cee94e2952bf5d21054f154ab
Vishwas Paikra <vishwaspaikra007@gmail.com>
2020-05-14 19:53:59 +0530

    working scroll bar indicator near chats, status, calls btn

--- src/App.js
+++ src/App.js
@@ -6,21 +6,26 @@ function App() {
 
   const [bodyRef, setBodyRef] = useState(null)
   const [scrollX, setScrollX] = useState(0)
+  const [marginLeft, setMarginLeft] = useState(10)
+  const [spanScrollX, setSpanScrollX] = useState(0)
+
   const scrollTo = (i)=> {
     bodyRef.current.style.scrollSnapType = "none"
     let scrollAnime;
+
     if(scrollAnime)
       clearInterval(scrollAnime)
+
     console.log(bodyRef);
     let start = bodyRef.current.scrollLeft
     const end = i*bodyRef.current.clientWidth
     const distance = end - start
     const pix = distance > 0 ? 15: -15
     let check = 0;
+
     if(bodyRef)
     {
       scrollAnime = setInterval(() => {
-        console.log(start)
         start+= pix 
         setScrollX(start)
        check += pix
@@ -39,9 +44,22 @@ function App() {
       bodyRef.current.scrollTo(scrollX,0)
   },[scrollX])
 
+  useEffect(() => {
+    if(bodyRef) {
+      bodyRef.current.addEventListener('scroll', e => {
+        setMarginLeft(10 + (bodyRef.current.scrollLeft/bodyRef.current.scrollWidth)*90)
+      })
+    }
+    // on component unmount so it doesnt update unnecessarily
+    return () => {
+      if(bodyRef)
+        bodyRef.current.removeEventListener('scroll')
+    }
+  },[bodyRef])
+
   return (
     <div className="App">
-      <Header scrollTo={scrollTo}/>
+      <Header scrollTo={scrollTo} marginLeft={marginLeft}/>
       <Body shareRef={ref => setBodyRef(ref)}/>
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,12 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import './header.css'
 
 export default function Header(props) {
+
+    let spanStyle = {
+        marginLeft: props.marginLeft + "%"
+    }
+
     return (
         <div>
             <div className="header">
@@ -16,6 +21,7 @@ export default function Header(props) {
                     <div onClick={() => props.scrollTo(1)}>STATUS</div>
                     <div onClick={() => props.scrollTo(2)}>CALLS</div>
                 </div>
+                <span className="indicator" style={spanStyle}></span>
             </div>
         </div>
     )
