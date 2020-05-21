c819ad56f4f2c17930688d7417237364dbe9f0cc
Vishwas Paikra <vishwaspaikra007@gmail.com>
2020-05-22 02:23:37 +0530

    room UI done

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Room.css'
 import ProfilePic from './ProfilePic'
+import {ReactComponent as Camera} from '../assets/camera.svg'
 export default function Room() {
     return (
         <div className={"room"}>
+            <div className="background-image"></div>
             <div className={"roomHeader"}>
                 <div className={"back"}><i className="return" /></div>
                 <div className="picContainer">
@@ -14,8 +16,16 @@ export default function Room() {
                 <div className="audioCall" ><i className="call" /></div>
                 <div className="menu-room"><i className="vertical-menu" /></div>
             </div>
-            <div className={"messages"} />
-            <div className={"create"} />
+            <div className={"messages"}></div>
+            <div className={"create"}>
+                <div className="chatInput">
+                    <div className="emoji"><i /></div>
+                    <input className="input" placeholder="Type a messsage"/>
+                    <div className="attachment"><i /></div>
+                    <div className="camera"><i /></div>
+                </div>
+                <div className="microphone"><i /></div>
+            </div>
         </div>
     )
 }
