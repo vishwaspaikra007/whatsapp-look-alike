c1e6f57e77a6a22d25bfb8230d106c1e23c13dda
Vishwas Paikra <vishwaspaikra007@gmail.com>
2024-01-10 15:01:16 +0530

    Updates

---
+++ precache-manifest.eba2e7b58f08ad3a5f95b0a669a12873.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c0f3d3a71e9a7699bc5d3dac673a5b",
+    "url": "/whatsapp-look-alike/index.html"
+  },
+  {
+    "revision": "2eda2b15360dad209c6a",
+    "url": "/whatsapp-look-alike/static/css/main.0ac2ec5e.chunk.css"
+  },
+  {
+    "revision": "5d52c0522c89c22e97ad",
+    "url": "/whatsapp-look-alike/static/js/2.3e46a4d0.chunk.js"
+  },
+  {
+    "revision": "f89ec0e947fe52779183e6ebdf1dc0ff",
+    "url": "/whatsapp-look-alike/static/js/2.3e46a4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eda2b15360dad209c6a",
+    "url": "/whatsapp-look-alike/static/js/main.e38d3e29.chunk.js"
+  },
+  {
+    "revision": "7c8e4e54caf08e20a087",
+    "url": "/whatsapp-look-alike/static/js/runtime-main.2b5a1d6c.js"
+  },
+  {
+    "revision": "f32f1158aea8be161f38f1b14ab00c38",
+    "url": "/whatsapp-look-alike/static/media/WelcomeBg.f32f1158.jpg"
+  },
+  {
+    "revision": "ba736b1ecc017b44b877d2a0a80d3eb3",
+    "url": "/whatsapp-look-alike/static/media/addGroup.ba736b1e.svg"
+  },
+  {
+    "revision": "8db9a78cb8b02457d290480121c5189e",
+    "url": "/whatsapp-look-alike/static/media/attachment.8db9a78c.svg"
+  },
+  {
+    "revision": "9e8a2898faedb7db9bf5638405cf81ae",
+    "url": "/whatsapp-look-alike/static/media/bg-chat-tile.9e8a2898.png"
+  },
+  {
+    "revision": "dbbcdac6bd35dafbd23ba40d6b2f453e",
+    "url": "/whatsapp-look-alike/static/media/call.dbbcdac6.svg"
+  },
+  {
+    "revision": "5cfe931c16fc42e5b516d28abe78390d",
+    "url": "/whatsapp-look-alike/static/media/camera copy.5cfe931c.svg"
+  },
+  {
+    "revision": "db60cc1c6c53d33c46299cf632fce48f",
+    "url": "/whatsapp-look-alike/static/media/camera.db60cc1c.svg"
+  },
+  {
+    "revision": "22024c0058eb746d04517d2a4afee40b",
+    "url": "/whatsapp-look-alike/static/media/createChat.22024c00.svg"
+  },
+  {
+    "revision": "d1688fa5fee0895721f8bf1318e30cc0",
+    "url": "/whatsapp-look-alike/static/media/doubleBlueTick.d1688fa5.svg"
+  },
+  {
+    "revision": "68d1416aadb7b98fa7e67c01b05193c3",
+    "url": "/whatsapp-look-alike/static/media/doubleTick.68d1416a.svg"
+  },
+  {
+    "revision": "36e41b998492db2d74479d955ab1216b",
+    "url": "/whatsapp-look-alike/static/media/emoji.36e41b99.svg"
+  },
+  {
+    "revision": "bbe7912941fb149d6e56f45cf0b16ed2",
+    "url": "/whatsapp-look-alike/static/media/microphone.bbe79129.svg"
+  },
+  {
+    "revision": "a871525ae307a44c5397b005711d6cbc",
+    "url": "/whatsapp-look-alike/static/media/pending.a871525a.svg"
+  },
+  {
+    "revision": "a8f61b5adaa2e8a08c87a1947fc5a295",
+    "url": "/whatsapp-look-alike/static/media/person.a8f61b5a.svg"
+  },
+  {
+    "revision": "748cb5261add158299612b76f89f316d",
+    "url": "/whatsapp-look-alike/static/media/return.748cb526.svg"
+  },
+  {
+    "revision": "bb348f14fef4baaffb3ced0fe884e520",
+    "url": "/whatsapp-look-alike/static/media/search.bb348f14.svg"
+  },
+  {
+    "revision": "09c7badf13cf7f5ca2912b388311741b",
+    "url": "/whatsapp-look-alike/static/media/send.09c7badf.svg"
+  },
+  {
+    "revision": "df8364ba1c4898d9872bec960c81c534",
+    "url": "/whatsapp-look-alike/static/media/singleTick.df8364ba.svg"
+  },
+  {
+    "revision": "e8af6707a73234c8cfb2fe075c927d55",
+    "url": "/whatsapp-look-alike/static/media/vertical-menu.e8af6707.svg"
+  },
+  {
+    "revision": "d7ba1e0c1307f0c840be52e8ba804c9d",
+    "url": "/whatsapp-look-alike/static/media/video-call.d7ba1e0c.svg"
+  }
+]);
